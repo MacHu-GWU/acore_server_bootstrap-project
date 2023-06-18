fb9711b95059f41d78a311d47ee826b171355564
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-06-18 01:49:54 -0400

    Update impl.py

--- acore_server_bootstrap/actions/s0_configure_ubuntu/impl.py
+++ acore_server_bootstrap/actions/s0_configure_ubuntu/impl.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import subprocess
+
 from ...logger import logger
 from .paths import path_20auto_upgrade_source, path_20auto_upgrade_target
 
@@ -7,4 +9,11 @@ from .paths import path_20auto_upgrade_source, path_20auto_upgrade_target
 @logger.start_and_end(msg="{func_name}")
 def disable_ubuntu_auto_upgrade():
     logger.info(f"Apply changes to {path_20auto_upgrade_target}")
-    path_20auto_upgrade_target.write_text(path_20auto_upgrade_source.read_text())
+
+    args = [
+        "sudo",
+        "cp",
+        f"{path_20auto_upgrade_source}",
+        f"{path_20auto_upgrade_target}",
+    ]
+    subprocess.run(args, check=True)
