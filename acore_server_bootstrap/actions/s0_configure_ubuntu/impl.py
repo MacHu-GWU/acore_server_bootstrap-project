e009ee06d29ea5a3a26599405a3abb6ad0466f97
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-06-26 11:19:22 -0400

    docs

--- acore_server_bootstrap/actions/s0_configure_ubuntu/impl.py
+++ acore_server_bootstrap/actions/s0_configure_ubuntu/impl.py
@@ -8,6 +8,14 @@ from .paths import path_20auto_upgrade_source, path_20auto_upgrade_target
 
 @logger.start_and_end(msg="{func_name}")
 def disable_ubuntu_auto_upgrade():
+    """
+    关闭 Ubuntu 的自动更新. 主要是为了防止 Ubuntu 更新 MySQL Client 的小版本. 因为游戏服务器
+    要求 MySQL Client 的版本跟服务器编译时的版本以及数据库的版本一摸一样.
+
+    Reference:
+
+    - https://askubuntu.com/questions/1322292/how-do-i-turn-off-automatic-updates-completely-and-for-real
+    """
     logger.info(f"Apply changes to {path_20auto_upgrade_target}")
 
     args = [
