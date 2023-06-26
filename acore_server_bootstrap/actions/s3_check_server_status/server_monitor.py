f3a54b0d2b617786ed9b3400302919296fb2f1e2
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-06-26 11:29:11 -0400

    [no ci]

--- acore_server_bootstrap/actions/s3_check_server_status/server_monitor.py
+++ acore_server_bootstrap/actions/s3_check_server_status/server_monitor.py
@@ -1,14 +1,16 @@
-#!/home/ubuntu/git_repos/acore_soap_app-project/.venv/bin/python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import time
 import subprocess
-from acore_paths.api import path_acore_soap_app_cli
 
 while 1:
     time.sleep(30)
     args = [
-        f"{path_acore_soap_app_cli}",
+        # make sure this path matches the definition in
+        # ``from acore_paths.api import path_acore_soap_app_cli``
+        f"/home/ubuntu/git_repos/acore_soap_app-project/.venv/bin/acsoap",
         "canned",
         "measure-server-status",
     ]
-    res = subprocess.run(args, capture_output=True, check=False)
+    res = subprocess.run(args)
