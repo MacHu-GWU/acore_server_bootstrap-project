eceb14289a14a75a297ebfc1917ad78220d04fdd
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2024-06-22 17:04:53 -0400

    update ci

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import sys
+import subprocess
+from pathlib import Path
 from acore_server_bootstrap import api
 
 
@@ -22,6 +25,39 @@ def test():
     _ = api.list_session
     _ = api.enter_worldserver
     _ = api.stop_server
+    _ = api.Remoter
+
+
+def test_cli():
+    path_cli = Path(sys.executable).parent.joinpath("acorebs")
+
+    def _test_command(action: str):
+        args = [f"{path_cli}", action, "-h"]
+        subprocess.run(args, capture_output=True, check=True)
+
+    actions = [
+        "hello",
+        "bootstrap_as_sudo",
+        "bootstrap",
+        "disable_ubuntu_auto_upgrade",
+        "setup_ec2_run_on_restart_script",
+        "create_database",
+        "create_user",
+        "update_realmlist",
+        "configure_db",
+        "apply_authserver_conf",
+        "apply_worldserver_conf",
+        "apply_mod_lua_engine_conf",
+        "apply_server_config",
+        "run_check_server_status_cron_job",
+        "stop_check_server_status_cron_job",
+        "run_server",
+        "list_session",
+        "enter_worldserver",
+        "stop_server",
+    ]
+    for action in actions:
+        _test_command(action)
 
 
 if __name__ == "__main__":
