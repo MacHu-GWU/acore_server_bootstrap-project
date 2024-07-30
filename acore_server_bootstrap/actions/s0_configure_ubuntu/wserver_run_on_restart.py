5a471b1f6af2ec5e7f69f7833734b4e3eb2c490d
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2024-07-30 00:24:56 -0400

    [no ci]

--- acore_server_bootstrap/actions/s0_configure_ubuntu/wserver_run_on_restart.py
+++ acore_server_bootstrap/actions/s0_configure_ubuntu/wserver_run_on_restart.py
@@ -23,7 +23,8 @@ args = [
 ]
 subprocess.run(args)
 
-# todo: the run_check_server_status_cron_job is no longer needed, consider remove it in the next release
+# todo: the run_check_server_status_cron_job is no longer needed, consider remove it
+# todo: and corresponding cli command, action implementation and document in the next release
 # --- run check server status cron job
 # args = [
 #     "/home/ubuntu/git_repos/acore_server_bootstrap-project/.venv/bin/acorebs",
